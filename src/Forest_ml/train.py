103a3a2da4adcb63b4c4c9cc910a0bee0d7a0845
Alexander Holubev <holubev@gmail.com>
2022-04-29 19:09:24 +0300

    Revert "Add test split ratio CLI option"
    This reverts commit 5e273461b304fa1dd318a81edb87c62664ad30fc.

--- src/Forest_ml/train.py
+++ src/Forest_ml/train.py
@@ -2,34 +2,16 @@ from pathlib import Path
 
 import click
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-
 
 @click.command()
 @click.option(
     "-d",
     "--dataset-path",
-    default="data/train.csv",
-    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+    default="data/train.csv",  
+    type=click.Path(exists=True, dir_okay=False, path_type=Path)
 )
-@click.option("--random-state", default=42, type=int)
-@click.option(
-    "--test-split-ratio",
-    default=0.2,
-    type=click.FloatRange(0, 1, min_open=True, max_open=True),
-)
-def train(dataset_path: Path, random_state: int, test_split_ratio: float) -> None:
+def train(
+    dataset_path: Path
+) -> None:
     dataset = pd.read_csv(dataset_path)
     click.echo(f"Dataset shape: {dataset.shape}.")
-    features = dataset.drop("Cover_Type", axis=1)
-    target = dataset["Cover_Type"]
-    features_train, features_val, target_train, target_val = train_test_split(
-        features, target, test_size=test_split_ratio, random_state=random_state
-    )
-    classifier = LogisticRegression(random_state=random_state).fit(
-        features_train, target_train
-    )
-    accuracy = accuracy_score(target_val, classifier.predict(features_val))
-    click.echo(f"Accuracy: {accuracy}.")
