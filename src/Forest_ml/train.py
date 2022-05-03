082b0043cc986b0e39e84a1442af3adcaf0815cb
wert1999 <holubev@gmail.com>
2022-05-03 21:09:28 +0300

    tune manual accuracy

--- src/Forest_ml/train.py
+++ src/Forest_ml/train.py
@@ -9,6 +9,9 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.decomposition import TruncatedSVD 
+from sklearn.manifold import TSNE
+
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
@@ -24,13 +27,35 @@ def fit_evaluate_model(model, X_train, y_train, X_valid, Y_valid):
 
 @click.command()
 @click.option(
+    "-a",
+    "--nested-cv",
+    help = "nested cross-validation",
+    default=False,
+    type=bool,
+)
+@click.option(
+    "-d",
+    "--decomposition",
+    help = "will be use TruncatedSVD",
+    default=False,
+    type=bool,
+)
+@click.option(
+    "-r",
+    "--n-components",
+    default=3,
+    help='n-components reduction',
+    type=int,
+    show_default=True,
+)
+@click.option(
     "-e",
     "--estimator",
     default="rf",
     type=click.Choice(['rf', 'knn']),
 )
 @click.option(
-    "-d",
+    "-p",
     "--dataset-path",
     default="data/train.csv",
     type=click.Path(exists=True, dir_okay=False, path_type=Path),
@@ -91,43 +116,56 @@ def fit_evaluate_model(model, X_train, y_train, X_valid, Y_valid):
     type=click.Choice(['uniform', 'distance']),
     show_default=True,
 )
+@click.option(
+    "-f",
+    "--max-features",
+    default="auto",
+    type=click.Choice(['auto', 'sqrt', 'log2'], case_sensitive=False) or int or float,
+    show_default=True,
+)
 
-def train(estimator, dataset_path: Path, save_model_path: Path, random_state: int, test_split_ratio: float,
-    n_estimators: int, criterion, max_depth: int, cv:int, use_scaler: bool, weights) -> None:
+def train(estimator, dataset_path: Path, save_model_path: Path, random_state: int,
+    test_split_ratio: float, n_estimators: int, criterion, max_depth: int, cv:int,
+    use_scaler: bool, weights, decomposition, n_components:int, max_features, nested_cv:bool) -> None:
     dataset = pd.read_csv(dataset_path)
     click.echo(f"Dataset shape: {dataset.shape}.")
     features = dataset.drop("Cover_Type", axis=1)
     target = dataset["Cover_Type"]
-    features_train, features_val, target_train, target_val = train_test_split(
-        features, target, test_size=test_split_ratio)
+    #features_train, features_val, target_train, target_val = train_test_split(
+     #   features, target, test_size=test_split_ratio)
 
     with mlflow.start_run():
+        if decomposition:
+            rdc = TruncatedSVD(n_components=n_components)
+            features = rdc.fit_transform(features)
+
         if estimator == "knn":
             knn = KNeighborsClassifier(n_neighbors=n_estimators, weights=weights)
 
             # create scaler
             if use_scaler:
+                click.echo("StandardScaler")
                 scaler = StandardScaler()
-                features_train_scaled = scaler.fit_transform(features_train, target_train)
-                features_valid_scaled = scaler.transform(features_val)
-                knn.fit(features_train_scaled, target_train)
-                y_predicted_knn = knn.predict(features_valid_scaled)
+                features_scaled = scaler.fit_transform(features)
+                #features_valid_scaled = scaler.transform(features_val)
+                knn.fit(features_scaled, target)
+               # y_predicted_knn = knn.predict(features_valid_scaled)
+                scores_accuracy = cross_val_score(knn,features_scaled, target, scoring='accuracy', cv=cv, n_jobs=-1)
+                scores_v_measure = cross_val_score(knn,features_scaled, target, scoring='v_measure_score', cv=cv, n_jobs=-1)
+                scores_f1_micro = cross_val_score(knn,features_scaled, target, scoring='f1_micro', cv=cv, n_jobs=-1)
             else:
-                knn.fit(features_train, target_train)
-                y_predicted_knn = knn.predict(features_val)    
-
-            knn_accuracy = accuracy_score(target_val, y_predicted_knn)
-            click.echo(f"Accuracy KNN model w/o CV: {knn_accuracy}.")
-
-            scores_accuracy = cross_val_score(knn,features, target, scoring='accuracy', cv=cv, n_jobs=-1)
-            scores_v_measure = cross_val_score(knn,features, target, scoring='v_measure_score', cv=cv, n_jobs=-1)
-            scores_f1_micro = cross_val_score(knn,features, target, scoring='f1_micro', cv=cv, n_jobs=-1)
+                click.echo("NO StandardScaler")
+                knn.fit(features, target)
+                scores_accuracy = cross_val_score(knn,features, target, scoring='accuracy', cv=cv, n_jobs=-1)
+                scores_v_measure = cross_val_score(knn,features, target, scoring='v_measure_score', cv=cv, n_jobs=-1)
+                scores_f1_micro = cross_val_score(knn,features, target, scoring='f1_micro', cv=cv, n_jobs=-1)
+            click.echo(f"best_params_: {scores_accuracy.best_params_}.")
 
             mlflow.log_param("model", estimator)
-            mlflow.log_param("n_neighbors", n_estimators)
+            mlflow.log_param("n_ngb./n_est.", n_estimators)
             mlflow.log_param("use_scaler", use_scaler)
             mlflow.log_param("weights", weights)
-            mlflow.log_metric("acc. w.o CV", knn_accuracy)
+            mlflow.log_param("decomp.", "T.SVD ("+str(n_components)+")" if decomposition else "-")
             mlflow.log_metric("accuracy", mean(scores_accuracy))
             mlflow.log_metric("v_measure", mean(scores_v_measure))
             mlflow.log_metric("f1_micro", mean(scores_f1_micro))
@@ -142,22 +180,18 @@ def train(estimator, dataset_path: Path, save_model_path: Path, random_state: in
         elif estimator == "rf":
 
             rf = RandomForestClassifier(n_estimators=n_estimators, criterion=criterion,
-                max_depth=max_depth, random_state=random_state) #,max_features=max_features
-
-            rf.fit(features_train, target_train)
-            y_predicted_rf = rf.predict(features_val)
-            rf_accuracy = accuracy_score(target_val, y_predicted_rf)
+                max_depth=max_depth, max_features=max_features, random_state=random_state)
 
             scores_accuracy = cross_val_score(rf,features, target, scoring='accuracy', cv=cv, n_jobs=-1)
             scores_v_measure = cross_val_score(rf,features, target, scoring='v_measure_score', cv=cv, n_jobs=-1)
             scores_f1_micro = cross_val_score(rf,features, target, scoring='f1_micro', cv=cv, n_jobs=-1)
-            click.echo(f"Accuracy RandomForest model w/o CV: {rf_accuracy}.")
 
             mlflow.log_param("model", estimator)
-            mlflow.log_param("n_estimators", n_estimators)
+            mlflow.log_param("n_ngb./n_est.", n_estimators)
             mlflow.log_param("criterion", criterion)
             mlflow.log_param("max_depth", max_depth)
-            mlflow.log_metric("acc. w.o CV", rf_accuracy)
+            mlflow.log_param("max_features", max_features)
+            mlflow.log_param("decomp.", "T.SVD ("+str(n_components)+")" if decomposition else "-")
             mlflow.log_metric("accuracy", mean(scores_accuracy))
             mlflow.log_metric("v_measure", mean(scores_v_measure))
             mlflow.log_metric("f1_micro", mean(scores_f1_micro))
@@ -167,14 +201,19 @@ def train(estimator, dataset_path: Path, save_model_path: Path, random_state: in
             dump(rf, rf_path)
             click.echo(f"Model is saved to {rf_path}.")
 
-
-
 '''
 @click.option(
     "-f",
     "--max-features",
     default="auto",
-    type=click.Choice(['auto', 'sqrt', 'log2', click.INT, click.FLOAT], case_sensitive=False),
+    type=int,
+    show_default=True,
+)
+@click.option(
+    "-f",
+    "--max-features",
+    default="auto",
+    type=int,
     show_default=True,
 )
 '''
