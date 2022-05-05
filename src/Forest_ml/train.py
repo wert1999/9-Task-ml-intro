d89e3900e9ad1a03b699db4ede55b9e96f2d72f0
wert1999 <holubev@gmail.com>
2022-05-06 00:20:17 +0300

    edit nestedCV

--- src/Forest_ml/train.py
+++ src/Forest_ml/train.py
@@ -4,8 +4,6 @@ from typing import Any
 from joblib import dump
 from numpy import mean
 import mlflow
-
-
 import mlflow.sklearn
 import click
 import pandas as pd
@@ -17,21 +15,15 @@ from sklearn.decomposition import TruncatedSVD
 from sklearn.manifold import TSNE
 from sklearn.model_selection import KFold
 from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
+
 #import sklearn.metrics as metrics
 
 from sklearn.metrics import accuracy_score,v_measure_score,hamming_loss, f1_score, precision_score
 from sklearn.metrics import make_scorer
 from sklearn.model_selection import train_test_split    
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score,cross_validate
 
-
-"""
-# function to train a given model, generate predictions, and return accuracy score
-def fit_evaluate_model(model, X_train, y_train, X_valid, Y_valid):
-    model.fit(X_train, y_train)
-    y_predicted = model.predict(X_valid)
-    return accuracy_score(Y_valid, y_predicted)
-"""
 
 
 @click.command()
@@ -159,113 +151,92 @@ def train(
     with mlflow.start_run():
         
         if nested_cv:
-            cv_inner = KFold(n_splits=cv, shuffle=True, random_state=random_state)
-            rf = RandomForestClassifier(random_state=random_state)
-            param: dict[str, Any] = dict() 
-            param["n_estimators"] = [10, 50]  # [100, 300, 500]
-            param["max_features"] = ['auto', 'sqrt', 'log2'] 
+            cv_inner = KFold(n_splits=3, shuffle=True, random_state=random_state)
+            model = RandomForestClassifier(random_state=random_state)
+            space = dict()
+            space['n_estimators'] = [10, 50]
+            space['max_features'] = ['auto', 'sqrt', 'log2', None] 
+            space['criterion'] = ["gini", "entropy"]
+            search = GridSearchCV(model, space, scoring='accuracy', n_jobs=-1, cv=cv_inner, refit=True)
+            cv_outer = KFold(n_splits=5, shuffle=True, random_state=1)
             scoring = {
-                "Accuracy": make_scorer(accuracy_score),
-                "V_measure": make_scorer(v_measure_score),
-                'f1_score' : make_scorer(f1_score, average = 'micro')}
-            #scoring = {"AUC": "roc_auc", "Accuracy": make_scorer(accuracy_score)}
-
-            search = GridSearchCV(
-                rf, param, scoring=scoring, refit=scoring, n_jobs=1, cv=cv_inner,
-            )
-
-            cv_outer = KFold(n_splits=3, shuffle=True, random_state=1)
-            scores = cross_val_score(
-                search, features, target, scoring="accuracy", cv=cv_outer, n_jobs=-1
-            )
-            result = search.fit(features, target)
-            #best_model = result.best_estimator_
-
-            # report performance
-
-            click.echo(f"Accuracy nested CV: {scores}.")
-            click.echo(f"Best param. nested CV: {result.best_params_}.")
-
-        # print('Accuracy: %.3f (%.3f)' % (mean(scores), std(scores)))
+                    "accuracy": make_scorer(accuracy_score),
+                    "V_measure": make_scorer(v_measure_score),
+                    "f1_score": make_scorer(f1_score, average='macro')
+                    }
+            scores = cross_validate(search, features, target, scoring=scoring, cv=cv_outer, n_jobs=-1, return_estimator = True)
+            #click.echo(f"scores nested CV: {scores}.")
+            scores_accuracy = scores['test_accuracy']
+            scores_v_measure = scores['test_V_measure']
+            scores_f1_macro = scores['test_f1_score']
+            rf = GridSearchCV(model, space, scoring='accuracy', n_jobs=-1, refit=True)
+            #click.echo(f"scores nested CV: {rf.get_params()}.")
+            rf_param = rf.get_params()
+            max_features = rf_param.get('max_features')
+            click.echo(f"max_features : {max_features}.")
+            rf_path = save_model_path
+            rf_path = Path(rf_path.parent, f"{rf_path.stem}_rf_nested{rf_path.suffix}")
+            dump(rf, rf_path)
+        
         else:
-
-            if decomposition:
-                rdc = TruncatedSVD(n_components=n_components)
-                features = rdc.fit_transform(features)
-
             if estimator == "knn":
-                knn = KNeighborsClassifier(n_neighbors=n_estimators, weights=weights)
                 if use_scaler:
-                    click.echo("StandardScaler")
-                    scaler = StandardScaler()
-                    features_scaled = scaler.fit_transform(features)
-                    # features_valid_scaled = scaler.transform(features_val)
-                    knn.fit(features_scaled, target)
-                    # y_predicted_knn = knn.predict(features_valid_scaled)
-                    scores_accuracy = cross_val_score(
-                        knn,
-                        features_scaled,
-                        target,
-                        scoring="accuracy",
-                        cv=cv,
-                        n_jobs=-1,
-                    )
-                    scores_v_measure = cross_val_score(
-                        knn,
-                        features_scaled,
-                        target,
-                        scoring="v_measure_score",
-                        cv=cv,
-                        n_jobs=-1,
-                    )
-                    scores_f1_micro = cross_val_score(
-                        knn,
-                        features_scaled,
-                        target,
-                        scoring="f1_micro",
-                        cv=cv,
-                        n_jobs=-1,
-                    )
+                    knn_pipe = Pipeline([('sc', StandardScaler()),
+                        ('knn', KNeighborsClassifier(n_neighbors=n_estimators, weights=weights))])
+                    click.echo("used StandardScaler")
+                    if decomposition:
+                        knn_pipe = Pipeline([('sc', StandardScaler()),
+                        ('svd', TruncatedSVD(n_components=n_components)),
+                        ('knn', KNeighborsClassifier(n_neighbors=n_estimators, weights=weights))])
+                        click.echo("used StandardScaler")
+                        click.echo("used TruncatedSVD")
+                elif decomposition:
+                    knn_pipe = Pipeline([
+                        ('svd', TruncatedSVD(n_components=n_components)),
+                        ('knn', KNeighborsClassifier(n_neighbors=n_estimators, weights=weights))])
+                    click.echo("used TruncatedSVD")
                 else:
-                    click.echo("NO StandardScaler")
-                    knn.fit(features, target)
-                    scores_accuracy = cross_val_score(
-                        knn, features, target, scoring="accuracy", cv=cv, n_jobs=-1
-                    )
-                    scores_v_measure = cross_val_score(
-                        knn,
-                        features,
-                        target,
-                        scoring="v_measure_score",
-                        cv=cv,
-                        n_jobs=-1,
-                    )
-                    scores_f1_micro = cross_val_score(
-                        knn, features, target, scoring="f1_micro", cv=cv, n_jobs=-1
-                    )
-                # click.echo(f"best_params_: {scores_accuracy.best_params_}.")
+                    knn_pipe = Pipeline([
+                        ('knn', KNeighborsClassifier(n_neighbors=n_estimators, weights=weights))])
 
-                mlflow.log_param("model", estimator)
-                mlflow.log_param("n_ngb./n_est.", n_estimators)
-                mlflow.log_param("use_scaler", use_scaler)
-                mlflow.log_param("weights", weights)
-                mlflow.log_param(
-                    "decomp.",
-                    "T.SVD (" + str(n_components) + ")" if decomposition else "-",
+                    # features_valid_scaled = scaler.transform(features_val)
+                    click.echo("used TruncatedSVD")
+
+                knn_pipe.fit(features, target)
+                # y_predicted_knn = knn.predict(features_valid_scaled)
+                scores_accuracy = cross_val_score(
+                    knn_pipe,
+                    features,
+                    target,
+                    scoring="accuracy",
+                    cv=cv,
+                    n_jobs=-1,
                 )
-                mlflow.log_metric("accuracy", mean(scores_accuracy))
-                mlflow.log_metric("v_measure", mean(scores_v_measure))
-                mlflow.log_metric("hamming_loss", mean(scores_f1_micro))
-
+                scores_v_measure = cross_val_score(
+                    knn_pipe,
+                    features,
+                    target,
+                    scoring="v_measure_score",
+                    cv=cv,
+                    n_jobs=-1,
+                )
+                scores_f1_macro = cross_val_score(
+                    knn_pipe,
+                    features,
+                    target,
+                    scoring="f1_macro",
+                    cv=cv,
+                    n_jobs=-1,
+                )
                 knn_path = save_model_path
                 knn_path = Path(
                     knn_path.parent, f"{knn_path.stem}_knn{knn_path.suffix}"
                 )
                 print("save_model_path", save_model_path)
                 print("knn_path", knn_path)
-                dump(knn, knn_path)
+                dump(knn_pipe, knn_path)
                 click.echo(f"Model is saved to {knn_path}.")
-
+                
             elif estimator == "rf":
 
                 rf = RandomForestClassifier(
@@ -275,35 +246,54 @@ def train(
                     max_features=max_features,
                     random_state=random_state,
                 )
+                if use_scaler:
+                    rf_pipe = Pipeline([('rf', rf)])
+                    click.echo("StandardScaler was ignored")
+                    if decomposition:
+                        rf_pipe = Pipeline([('svd', TruncatedSVD(n_components=n_components)),
+                        ('rf', rf)])
+                elif decomposition:
+                    rf_pipe = Pipeline([
+                        ('svd', TruncatedSVD(n_components=n_components)),
+                        ('rf', rf)])
+                else:
+                    rf_pipe = Pipeline([
+                        ('rf', rf)])
 
                 scores_accuracy = cross_val_score(
-                    rf, features, target, scoring="accuracy", cv=cv, n_jobs=-1
+                    rf_pipe, features, target, scoring="accuracy", cv=cv, n_jobs=-1
                 )
                 scores_v_measure = cross_val_score(
-                    rf, features, target, scoring="v_measure_score", cv=cv, n_jobs=-1
+                    rf_pipe, features, target, scoring="v_measure_score", cv=cv, n_jobs=-1
                 )
-                scores_f1_micro = cross_val_score(
-                    rf, features, target, scoring="f1_micro", cv=cv, n_jobs=-1
+                scores_f1_macro = cross_val_score(
+                    rf_pipe, features, target, scoring="f1_macro", cv=cv, n_jobs=-1
                 )
-
-                mlflow.log_param("model", estimator)
-                mlflow.log_param("n_ngb./n_est.", n_estimators)
-                mlflow.log_param("criterion", criterion)
-                mlflow.log_param("max_depth", max_depth)
-                mlflow.log_param("max_features", max_features)
-                mlflow.log_param(
-                    "decomp.",
-                    "T.SVD (" + str(n_components) + ")" if decomposition else "-",
-                )
-                mlflow.log_metric("accuracy", mean(scores_accuracy))
-                mlflow.log_metric("v_measure", mean(scores_v_measure))
-                mlflow.log_metric("f1_micro", mean(scores_f1_micro))
-
                 rf_path = save_model_path
                 rf_path = Path(rf_path.parent, f"{rf_path.stem}_rf{rf_path.suffix}")
-                dump(rf, rf_path)
+                dump(rf_pipe, rf_path)
                 click.echo(f"Model is saved to {rf_path}.")
+        
+       # mlflow.sklearn.log_model(knn_path, "model", registered_model_name="KNN")
 
+        mlflow.log_param("nested_cv", nested_cv)
+        mlflow.log_param("_model", estimator)
+        mlflow.log_param("n_ngb./n_est.", n_estimators)
+        mlflow.log_param("criterion", criterion if estimator=="rf" else "-")
+        mlflow.log_param("weights", weights if estimator=="knn" else "-")
+        mlflow.log_param("use_scaler", use_scaler if (not nested_cv) and (estimator != "rf") else "not use")
+        mlflow.log_param("max_depth", max_depth if estimator=="rf" else "-")
+        mlflow.log_param("max_features", max_features if estimator=="rf" else "-")
+        mlflow.log_param(
+            "decomp.",
+            "T.SVD (" + str(n_components) + ")" if decomposition and not nested_cv else "-",
+        )
+        mlflow.log_metric("accuracy", mean(scores_accuracy))
+        mlflow.log_metric("v_measure", mean(scores_v_measure))
+        mlflow.log_metric("f1_macro", mean(scores_f1_macro))
+
+
+                
 
 """
 @click.option(
@@ -321,3 +311,31 @@ def train(
     show_default=True,
 )
 """
+'''
+cv_inner = KFold(n_splits=cv, shuffle=True, random_state=random_state)
+rf = RandomForestClassifier(random_state=random_state)
+param: dict[str, Any] = dict() 
+param["n_estimators"] = [10, 50]  # [100, 300, 500]
+param["max_features"] = ['auto', 'sqrt', 'log2'] 
+scoring = {
+    "accuracy": make_scorer(accuracy_score),
+    "V_measure": make_scorer(v_measure_score),
+    "f1_score ": make_scorer(f1_score, average='macro')
+    }
+
+search = GridSearchCV(
+    rf, param, scoring=scoring, refit="accuracy", n_jobs=1, cv=cv_inner,
+)
+
+cv_outer = KFold(n_splits=3, shuffle=True, random_state=1)
+scores = cross_val_score(
+    search, features, target, scoring="accuracy", cv=cv_outer, n_jobs=-1
+)
+result = search.fit(features, target)
+#best_model = result.best_estimator_
+
+# report performance
+
+click.echo(f"Accuracy nested CV: {scores}.")
+click.echo(f"Best param. nested CV: {result.best_params_}.")
+'''
